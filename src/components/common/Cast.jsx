56682515770382147952a6f0c59c41a6bccceac8
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-08-18 17:42:07 +0530

    Enhanced responsiveness across routes; adjusted UI for title images and background gradients; fixed overflow issues and alignment for various components.

--- src/components/common/Cast.jsx
+++ src/components/common/Cast.jsx
@@ -1,75 +1,105 @@
-import React from 'react';
+import React from "react";
 import NoImg from "../../assets/boy.png";
 
 const Cast = ({ cast, createdBy }) => {
-   
-    // Ensure cast is defined and filter members with profile images, limiting to 13
-    const castWithImages = (cast && Array.isArray(cast) ? cast.filter(member => member.profile_path) : []).slice(0, 13);
+  // Ensure cast is defined and filter members with profile images, limiting to 13
+  const castWithImages = (
+    cast && Array.isArray(cast)
+      ? cast.filter((member) => member.profile_path)
+      : []
+  ).slice(0, 13);
 
-    // Ensure createdBy is defined and filter creators with profile images, limiting to 13
-    const creatorsWithImages = (createdBy && Array.isArray(createdBy) ? createdBy.filter(creator => creator.profile_path) : []).slice(0, 13);
+  // Ensure createdBy is defined and filter creators with profile images, limiting to 13
+  const creatorsWithImages = (
+    createdBy && Array.isArray(createdBy)
+      ? createdBy.filter((creator) => creator.profile_path)
+      : []
+  ).slice(0, 13);
 
-    // Default message when no cast members or creators are available
-    if (castWithImages.length === 0) {
-        castWithImages.push({
-            id: 0,
-            name: "No cast information available",
-            profile_path: NoImg,
-        });
-    }
+  if (castWithImages.length === 0) {
+    castWithImages.push({
+      id: 0,
+      name: "No cast information available",
+      profile_path: NoImg,
+    });
+  }
 
-    return (
-        <div className="w-full mx-auto mt-10">
-            <h2 className="text-3xl font-bold text-white mb-9 ml-9">Cast</h2>
-            <div className="flex flex-wrap lg:gap-5 gap-3 lg:justify-center">
-                {castWithImages.map((member) => (
-                    <div key={member.id} className="w-[30%] sm:w-[50%] md:w-[30%] lg:w-[20%] xl:w-[15%] flex flex-col items-center space-y-5">
-                        <div className="w-[100px] h-[100px] rounded-full overflow-hidden shadow-lg">
-                            <img
-                                className="w-full h-full object-cover object-center"
-                                src={member.profile_path === NoImg ? NoImg : `https://image.tmdb.org/t/p/w500${member.profile_path}`}
-                                alt={member.name}
-                                onError={(e) => {
-                                    e.target.onerror = null;
-                                    e.target.src = NoImg;
-                                }}
-                            />
-                        </div>
-                        <div className='flex flex-col items-center'>
-                            <p className="text-white font-semibold mt-2 text-center">{member.name}</p>
-                            {member.character && <p className="text-white mt-1 w-full text-sm opacity-75 text-center">{member.character.slice(0, 20)}</p>}
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="w-full mx-auto">
+      <h2 className="text-3xl font-bold text-white mb-9 ml-9">Cast</h2>
+      <div className="flex flex-wrap lg:gap-5 gap-3 justify-center">
+        {castWithImages.map((member) => (
+          <div
+            key={member.id}
+            className="w-[30%] sm:w-[50%] md:w-[30%] lg:w-[20%] xl:w-[15%] flex flex-col items-center space-y-5"
+          >
+            <div className="w-[100px] h-[100px] rounded-full overflow-hidden shadow-lg">
+              <img
+                className="w-full h-full object-cover object-center"
+                src={
+                  member.profile_path === NoImg
+                    ? NoImg
+                    : `https://image.tmdb.org/t/p/w500${member.profile_path}`
+                }
+                alt={member.name}
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = NoImg;
+                }}
+              />
             </div>
+            <div className="flex flex-col items-center">
+              <p className="text-white font-semibold mt-2 text-center">
+                {member.name}
+              </p>
+              {member.character && (
+                <p className="text-white mt-1 w-full text-sm opacity-75 text-center">
+                  {member.character.slice(0, 20)}
+                </p>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
 
-            {creatorsWithImages.length > 0 && (
-                <>
-                    <h2 className="text-3xl font-bold text-white mb-9 mt-12 ml-9">Creators</h2>
-                    <div className="flex flex-wrap lg:gap-5 gap-3 lg:justify-center">
-                        {creatorsWithImages.map((creator) => (
-                            <div key={creator.id} className="w-[30%] sm:w-[50%] md:w-[30%] lg:w-[20%] xl:w-[15%] flex flex-col items-center space-y-5">
-                                <div className="w-[100px] h-[100px] rounded-full overflow-hidden shadow-lg">
-                                    <img
-                                        className="w-full h-full object-cover object-center"
-                                        src={creator.profile_path === NoImg ? NoImg : `https://image.tmdb.org/t/p/w500${creator.profile_path}`}
-                                        alt={creator.name}
-                                        onError={(e) => {
-                                            e.target.onerror = null;
-                                            e.target.src = NoImg;
-                                        }}
-                                    />
-                                </div>
-                                <div className='flex flex-col items-center'>
-                                    <p className="text-white font-semibold mt-2 text-center">{creator.name}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
-        </div>
-    );
+      {creatorsWithImages.length > 0 && (
+        <>
+          <h2 className="text-3xl font-bold text-white mb-9 mt-12 ml-9">
+            Creators
+          </h2>
+          <div className="flex flex-wrap lg:gap-5 gap-3 lg:justify-center">
+            {creatorsWithImages.map((creator) => (
+              <div
+                key={creator.id}
+                className="w-[30%] sm:w-[50%] md:w-[30%] lg:w-[20%] xl:w-[15%] flex flex-col items-center space-y-5"
+              >
+                <div className="w-[100px] h-[100px] rounded-full overflow-hidden shadow-lg">
+                  <img
+                    className="w-full h-full object-cover object-center"
+                    src={
+                      creator.profile_path === NoImg
+                        ? NoImg
+                        : `https://image.tmdb.org/t/p/w500${creator.profile_path}`
+                    }
+                    alt={creator.name}
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.src = NoImg;
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col items-center">
+                  <p className="text-white font-semibold mt-2 text-center">
+                    {creator.name}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Cast;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,163 +1,163 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from '../utils/axios';
-import Cards from '../components/common/Card';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Loader from '../components/common/Loader';
-import DropDown from '../components/DropDown';
-import { HiOutlineRefresh } from 'react-icons/hi';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "../utils/axios";
+import Cards from "../components/common/Card";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from "../components/common/Loader";
+import DropDown from "../components/DropDown";
+import { HiOutlineRefresh } from "react-icons/hi";
 
 const API_KEY = import.meta.env.VITE_REACT_APP_TOKEN;
 
 function Movie() {
-    document.title = "MovieFlex | Movies";
-    const [movies, setMovies] = useState([]);
-    const [page, setPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
-    const [loading, setLoading] = useState(true);
-    const [genres, setGenres] = useState([]);
-    const [selectedGenres, setSelectedGenres] = useState([]);
-    const [year, setYear] = useState(new Date().getFullYear());
-    const [category, setCategory] = useState("movie");
-    const [resetKey, setResetKey] = useState(0); 
+  document.title = "MovieFlex | Movies";
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [category, setCategory] = useState("movie");
+  const [resetKey, setResetKey] = useState(0);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetchGenres();
-    }, [category, resetKey]);
+  useEffect(() => {
+    fetchGenres();
+  }, [category, resetKey]);
 
-    useEffect(() => {
-        fetchMovies();
-    }, [page, selectedGenres, year, category]);
+  useEffect(() => {
+    fetchMovies();
+  }, [page, selectedGenres, year, category]);
 
-    const fetchMovies = async () => {
-        try {
-            const response = await axios.get(`/discover/${category}`, {
-                params: {
-                    api_key: API_KEY,
-                    page: page,
-                    sort_by: 'popularity.desc',
-                    include_adult: false,
-                    include_video: false,
-                    language: 'en-US',
-                    with_genres: selectedGenres.join(','),
-                    primary_release_year: year
-                }
-            });
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(`/discover/${category}`, {
+        params: {
+          api_key: API_KEY,
+          page: page,
+          sort_by: "popularity.desc",
+          include_adult: false,
+          include_video: false,
+          language: "en-US",
+          with_genres: selectedGenres.join(","),
+          primary_release_year: year,
+        },
+      });
 
-            setMovies(prevMovies => [...prevMovies, ...response.data.results]);
-            setHasMore(response.data.page < response.data.total_pages);
-            setLoading(false);
+      setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
+      setHasMore(response.data.page < response.data.total_pages);
+      setLoading(false);
 
-            console.log("fetchMovies", response.data)
-        } catch (error) {
-            console.error("Failed to fetch movies", error);
-            setLoading(false);
-        }
-    };
+      console.log("fetchMovies", response.data);
+    } catch (error) {
+      console.error("Failed to fetch movies", error);
+      setLoading(false);
+    }
+  };
 
-    const fetchGenres = async () => {
-        try {
-            const response = await axios.get(`/genre/${category}/list`, {
-                params: {
-                    api_key: API_KEY,
-                    language: 'en-US'
-                }
-            });
+  const fetchGenres = async () => {
+    try {
+      const response = await axios.get(`/genre/${category}/list`, {
+        params: {
+          api_key: API_KEY,
+          language: "en-US",
+        },
+      });
 
-            setGenres(response.data.genres);
-            console.log("fetchGenres", response.data)
-        } catch (error) {
-            console.error("Failed to fetch genres", error);
-        }
-    };
+      setGenres(response.data.genres);
+    } catch (error) {
+      console.error("Failed to fetch genres", error);
+    }
+  };
 
-    const onGenresChange = (selectedGenreId) => {
-        setSelectedGenres([selectedGenreId]);
-        setMovies([]);
-        setPage(1);
-    };
+  const onGenresChange = (selectedGenreId) => {
+    setSelectedGenres([selectedGenreId]);
+    setMovies([]);
+    setPage(1);
+  };
 
-    const onYearChange = (selectedYear) => {
-        setYear(selectedYear);
-        setMovies([]);
-        setPage(1);
-    };
+  const onYearChange = (selectedYear) => {
+    setYear(selectedYear);
+    setMovies([]);
+    setPage(1);
+  };
 
-    const onCategoryChange = (selectedCategory) => {
-        setCategory(selectedCategory.toLowerCase());
-        setSelectedGenres([]);
-        setMovies([]);
-        setPage(1);
-    };
+  const onCategoryChange = (selectedCategory) => {
+    setCategory(selectedCategory.toLowerCase());
+    setSelectedGenres([]);
+    setMovies([]);
+    setPage(1);
+  };
 
-    const handleReset = () => {
-        setGenres([]);
-        setSelectedGenres([]);
-        setYear(new Date().getFullYear());
-        setPage(1);
-        setCategory("movie");
-        setResetKey(prevKey => prevKey + 1);
-        setMovies([]);
-    };
+  const handleReset = () => {
+    setGenres([]);
+    setSelectedGenres([]);
+    setYear(new Date().getFullYear());
+    setPage(1);
+    setCategory("movie");
+    setResetKey((prevKey) => prevKey + 1);
+    setMovies([]);
+  };
 
-    const CURRENT_YEAR = new Date().getFullYear();
-    const YEARS = Array.from(new Array(20), (val, index) => CURRENT_YEAR - index);
+  const CURRENT_YEAR = new Date().getFullYear();
+  const YEARS = Array.from(new Array(20), (val, index) => CURRENT_YEAR - index);
 
-    return (
-        loading ? (
-            <div className="flex justify-center items-center h-screen">
-                <Loader />
-            </div>
-        ) : (
-            <div className="w-full mx-auto ">
-                <div className="w-full flex lg:flex-row md:w-[70%] flex-col space-x-5 mx-auto mt-32">
-                    <DropDown
-                        key={`category-${resetKey}`}
-                        title="Categories"
-                        options={[
-                            { id: "movie", name: "Movies" },
-                            { id: "tv", name: "TV Shows" }
-                        ]}
-                        func={onCategoryChange}
-                        defaultValue={{ id: "movie", name: "Movies" }}
-                    />
-                    <DropDown
-                        key={`genres-${resetKey}`}
-                        title="Genres"
-                        options={genres}
-                        func={onGenresChange}
-                        defaultValue="0"
-                    />
-                    <DropDown
-                        key={`year-${resetKey}`}
-                        title="Year"
-                        options={YEARS.map(year => ({ id: year, name: year.toString() }))}
-                        func={onYearChange}
-                        defaultValue={new Date().getFullYear().toString()}
-                    />
+  return loading ? (
+    <div className="flex justify-center items-center h-screen">
+      <Loader />
+    </div>
+  ) : (
+    <div className="w-full mx-auto px-4 mt-32">
+      <div className="max-w-full flex flex-col justify-center lg:flex-row lg:space-x-4 lg:space-y-0 space-y-4  overflow-x-hidden">
+        <DropDown
+          key={`category-${resetKey}`}
+          title="Categories"
+          options={[
+            { id: "movie", name: "Movies" },
+            { id: "tv", name: "TV Shows" },
+          ]}
+          func={onCategoryChange}
+          defaultValue={{ id: "movie", name: "Movies" }}
+          className="flex-shrink-0 w-full lg:w-auto"
+        />
+        <DropDown
+          key={`genres-${resetKey}`}
+          title="Genres"
+          options={genres}
+          func={onGenresChange}
+          defaultValue="0"
+          className="flex-shrink-0 w-full lg:w-auto"
+        />
+        <DropDown
+          key={`year-${resetKey}`}
+          title="Year"
+          options={YEARS.map((year) => ({ id: year, name: year.toString() }))}
+          func={onYearChange}
+          defaultValue={new Date().getFullYear().toString()}
+          className="flex-shrink-0 w-full lg:w-auto"
+        />
 
-                    <button
-                        className="bg-red-400 text-white px-4 py-2 rounded"
-                        onClick={handleReset}
-                    >
-                        <HiOutlineRefresh className="inline-block w-5 h-5 mr-2" />
-                        Reset
-                    </button>
-                </div>
+        <button
+          className="bg-red-400 text-white px-4 py-2 rounded flex-shrink-0 w-full lg:w-auto"
+          onClick={handleReset}
+        >
+          <HiOutlineRefresh className="inline-block w-5 h-5 mr-2" />
+          Reset
+        </button>
+      </div>
 
-                <InfiniteScroll
-                    dataLength={movies.length}
-                    next={() => setPage(page + 1)}
-                    hasMore={hasMore}
-                    loader={<Loader />}
-                >
-                    <Cards data={movies} title={"movie"} />
-                </InfiniteScroll>
-            </div>
-        )
-    );
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={() => setPage(page + 1)}
+        hasMore={hasMore}
+        loader={<Loader />}
+      >
+        <Cards data={movies} title={"movie"} />
+      </InfiniteScroll>
+    </div>
+  );
 }
 
 export default Movie;
